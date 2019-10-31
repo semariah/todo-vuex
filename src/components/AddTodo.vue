f3c1ce3610ccee56ed7bf77beb714178381b5510
sk <semaria.eyzac@gmail.com>
2019-10-31 13:18:03 -0700

    import and add the addtodo component insidee the app component and create the form of the AddTodo component

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h3>Add Todo</h3>
+    <div>
+      <form>
+        <input type="text" placeholder="Add Todo..." />
+      </form>
+    </div>
   </div>
 </template>
 <script>
